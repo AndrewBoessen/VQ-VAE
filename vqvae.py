27bf070fefddd00482739e63d07ae0e6a7950f81
andrew <boessena@bc.edu>
2024-10-15 00:08:22 -0400

    add requirements

--- vqvae.py
+++ vqvae.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torch.utils.data import Dataloader, randon_split
+from torch.utils.data import DataLoader, random_split
 
 
 class VectorQuantizeEMA(nn.Module):
