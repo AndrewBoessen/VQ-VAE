f65ddcea42afb8b0191d4651b49f8754234beaa9
Andrew Boessen <boessena@bc.edu>
2024-10-20 23:07:07 -0400

    format

--- train.py
+++ train.py
@@ -38,10 +38,13 @@ def read_config(file_path):
                 training=training,
             )
         except yaml.YAMLError as e:
-            print(f"Error reading the YAML file: {from torch import
+            print(f"Error reading the YAML file: {e}")
+            return None
+
+
 def main():
-    config_path="config.yaml"  # Adjust this path as needed
-    config=read_config(config_path)
+    config_path = "config.yaml"  # Adjust this path as needed
+    config = read_config(config_path)
     if config:
         print("Model Configuration:")
         print(f"Batch Size: {config.batch_size}")
@@ -55,33 +58,32 @@ def main():
     else:
         print("Failed to read the configuration file.")
 
-    data_path="skiing_observations.npy"
+    data_path = "skiing_observations.npy"
 
-    data=np.load(data_path)
-    data=data.astype(np.float32) / 255.0  # convert data to float and norm
+    data = np.load(data_path)
+    data = data.astype(np.float32) / 255.0  # convert data to float and norm
 
     print("\n--- Loading Data ---")
     print(f"Loaded Data: {data.shape[0]}")
 
-    train_data, test_data=random_split(data, [0.8, 0.2])
+    train_data, test_data = random_split(data, [0.8, 0.2])
 
     print(f"Train Data: {len(train_data)}")
     print(f"Test Data: {len(test_data)}")
 
-    data_variance=np.var(data[train_data.indices])
+    data_variance = np.var(data[train_data.indices])
 
-    device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     # DataLoaders
-    training_loader=DataLoader(
+    training_loader = DataLoader(
         train_data,
         batch_size=config.batch_size,
         shuffle=True,
         pin_memory=True,
     )
 
-
     # VQ-VAE Model
-    model=VQVAE(
+    model = VQVAE(
         config.architecture.num_hiddens,
         config.architecture.num_residual_layers,
         config.architecture.num_residual_hiddens,
@@ -91,60 +93,60 @@ def main():
         config.training.decay,
     ).to(device)
 
-    optimizer=optim.Adam(
+    optimizer = optim.Adam(
         params=model.parameters(), lr=config.training.learning_rate, amsgrad=False
     )
 
     # TensorBoard setup
-    current_time=datetime.now().strftime("%b%d_%H-%M-%S")
-    log_dir=os.path.join("runs", current_time)
-    writer=SummaryWriter(log_dir)
+    current_time = datetime.now().strftime("%b%d_%H-%M-%S")
+    log_dir = os.path.join("runs", current_time)
+    writer = SummaryWriter(log_dir)
 
     print("\n--- Training ---")
     # Training Loop
-    pbar=tqdm(range(config.num_training_updates))
+    pbar = tqdm(range(config.num_training_updates))
     for i in pbar:
-        data=next(iter(training_loader))  # current batch of data
-        data=torch.Tensor(data).to(device)
+        data = next(iter(training_loader))  # current batch of data
+        data = torch.Tensor(data).to(device)
         # convert from B, H, W, C -> B, C, H, W
-        data=data.permute(0, 3, 1, 2).contiguous()
+        data = data.permute(0, 3, 1, 2).contiguous()
 
         optimizer.zero_grad()
 
         if i == config.training.pretrain_steps:
             # K Means Cluster to initialize discrete embeddings
             with torch.no_grad():
-                embeddings=model.encode(data)
+                embeddings = model.encode(data)
 
-            embeddings=(
+            embeddings = (
                 embeddings.permute(0, 2, 3, 1)
                 .contiguous()
                 .reshape(-1, config.architecture.embedding_dim)
             )
 
-            np_e=embeddings.cpu().detach().numpy()
+            np_e = embeddings.cpu().detach().numpy()
 
-            n_clusters=config.architecture.num_embeddings
-            kmeans=KMeans(n_clusters)
+            n_clusters = config.architecture.num_embeddings
+            kmeans = KMeans(n_clusters)
             kmeans.fit(np_e)
 
-            cluster_centers=torch.from_numpy(
+            cluster_centers = torch.from_numpy(
                 kmeans.cluster_centers_).to(device)
 
             model.set_embeddings(cluster_centers)
 
         # Pretrain without vector quantizer
         if i < config.training.pretrain_steps:
-            data_recon=model.pretrain(data)
-            loss=nn.functional.mse_loss(data_recon, data) / data_variance
-            recon_error=loss
-            vq_loss=torch.Tensor([0.0])
-            perplexity=torch.Tensor([0.0])
+            data_recon = model.pretrain(data)
+            loss = nn.functional.mse_loss(data_recon, data) / data_variance
+            recon_error = loss
+            vq_loss = torch.Tensor([0.0])
+            perplexity = torch.Tensor([0.0])
         else:
-            vq_loss, data_recon, perplexity=model(data)
-            recon_error=nn.functional.mse_loss(
+            vq_loss, data_recon, perplexity = model(data)
+            recon_error = nn.functional.mse_loss(
                 data_recon, data) / data_variance
-            loss=recon_error + vq_loss
+            loss = recon_error + vq_loss
 
         loss.backward()
         optimizer.step()
@@ -159,7 +161,7 @@ def main():
 
         # Save model checkpoint every 1000 updates
         if (i + 1) % 1000 == 0:
-            checkpoint_path=os.path.join(
+            checkpoint_path = os.path.join(
                 "checkpoints", f"model_checkpoint_{i+1}.pth")
             os.makedirs("checkpoints", exist_ok=True)
             torch.save(
